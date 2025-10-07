ed010b79214ae4712a372cd2a5473ea24ef8394e
richie-south <richard.soderman@gmail.com>
2025-10-07 09:37:22 +0200

    feat: support for example node

--- src/index.test.ts
+++ src/index.test.ts
@@ -96,4 +96,124 @@ describe('convert', () => {
       ],
     })
   })
+
+  it('schema to json 2', () => {
+    const schema = {
+      type: 'object',
+      properties: {
+        orderId: {
+          type: 'string',
+        },
+        customer: {
+          type: 'object',
+          properties: {
+            id: {
+              type: 'integer',
+            },
+            name: {
+              type: 'string',
+            },
+            email: {
+              type: 'string',
+            },
+          },
+        },
+        items: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              productId: {
+                type: 'integer',
+              },
+              name: {
+                type: 'string',
+              },
+              price: {
+                type: 'number',
+              },
+              quantity: {
+                type: 'integer',
+              },
+            },
+          },
+        },
+        total: {
+          type: 'number',
+        },
+        paid: {
+          type: 'boolean',
+        },
+        shippedAt: {
+          type: 'null',
+        },
+      },
+    }
+
+    const result = getJsonStructure(schema)
+
+    expect(result).toEqual({
+      orderId: 'string',
+      customer: {
+        id: 'integer',
+        name: 'string',
+        email: 'string',
+      },
+      items: [
+        {
+          productId: 'integer',
+          name: 'string',
+          price: 'number',
+          quantity: 'integer',
+        },
+      ],
+      total: 'number',
+      paid: 'boolean',
+      shippedAt: 'null',
+    })
+  })
+
+  it('schema to json with example', () => {
+    const schema = {
+      type: 'object',
+      properties: {
+        customer: {
+          type: 'object',
+          properties: {
+            id: {
+              type: 'integer',
+              example: 17,
+              examples: [17],
+            },
+            name: {
+              type: 'string',
+              example: 'John Doe',
+              examples: ['John Doe'],
+            },
+            email: {
+              type: 'string',
+              example: 'john@example.com',
+              examples: ['john@example.com'],
+            },
+          },
+          required: ['id', 'name', 'email'],
+          additionalProperties: false,
+        },
+      },
+      required: ['customer'],
+      additionalProperties: false,
+    }
+
+    const result = getJsonStructure(schema, {
+      useExample: true,
+    })
+
+    expect(result).toEqual({
+      customer: {
+        id: 17,
+        name: 'John Doe',
+        email: 'john@example.com',
+      },
+    })
+  })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,65 @@
 import {compileSchema, JsonSchema, SchemaNode} from 'json-schema-library'
 
-function walkCompiledNode(node: SchemaNode, built = {}): object {
+type Options = {
+  useExample?: boolean
+}
+
+const defaultOptions: Options = {
+  useExample: false,
+}
+
+function getExample(node: SchemaNode) {
+  if (node.schema.example) {
+    return node.schema.example
+  } else if (
+    Array.isArray(node.schema.examples) &&
+    node.schema.examples.length > 0
+  ) {
+    return node.schema.examples[0]
+  }
+
+  return node.schema.type
+}
+
+function walkCompiledNode(
+  node: SchemaNode,
+  built = {},
+  options: Options = defaultOptions,
+): object {
   if (node.schema.properties && node.schema.type === 'object') {
     for (const key in node.schema.properties) {
       const {node: childNode} = node.getNode(key)
       if (childNode) {
         built = {
           ...built,
-          [key]: walkCompiledNode(childNode),
+          [key]: walkCompiledNode(childNode, {}, options),
         }
       }
     }
   }
 
   if (node.schema.type !== 'object' && node.schema.type !== 'array') {
+    if (options.useExample) {
+      return getExample(node)
+    }
+
     return node.schema.type
   }
 
   if (node.schema.items && node.schema.type === 'array') {
     const {node: itemNode} = node.getNode(0 as any)
     if (itemNode) {
-      return [walkCompiledNode(itemNode.items as SchemaNode)]
+      return [walkCompiledNode(itemNode.items as SchemaNode, {}, options)]
     }
   }
 
   return built
 }
 
-export function getJsonStructure(schema: JsonSchema) {
+export function getJsonStructure(
+  schema: JsonSchema,
+  options: Options = defaultOptions,
+) {
   const data = compileSchema(schema)
-  return walkCompiledNode(data)
+  return walkCompiledNode(data, {}, options)
 }
